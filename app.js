333b496b0753f17110ac606e8a8fca0768b7a986
restrada <rene1tres@gmail.com>
2020-10-30 19:24:00 -0600

    enable all cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let createError = require('http-errors');
 let express = require('express');
+let cors = require('cors');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
@@ -12,6 +13,9 @@ let app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// enable all cors
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
